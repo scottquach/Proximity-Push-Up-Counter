87bbb3af468b5ea922edeac3665e7d9df2022a25
scott quach <scottqglobal@gmail.com>
2017-09-09 13:00:35 -0700

    MediaPlayer calls when null bug fix

--- app/src/main/java/com/awesome/scottquach/proximitypush_upcounter/MainActivity.java
+++ app/src/main/java/com/awesome/scottquach/proximitypush_upcounter/MainActivity.java
@@ -35,7 +35,6 @@ import java.util.Locale;
 public class MainActivity extends Activity implements SensorEventListener {
 
 
-
     private TextView countDisplay;
     private Switch vibrateSwitch;
     private Switch soundSwitch;
@@ -104,43 +103,39 @@ public class MainActivity extends Activity implements SensorEventListener {
         sharedPref = getSharedPreferences("formattingFile", MODE_PRIVATE);
         editor = sharedPref.edit();
 
-        settingPref = getSharedPreferences("settingsFile",MODE_PRIVATE);
+        settingPref = getSharedPreferences("settingsFile", MODE_PRIVATE);
         prefEditor = settingPref.edit();
 
         goalSharedPref = getSharedPreferences("goalFile", MODE_PRIVATE);
         goalValue = goalSharedPref.getInt("goalValue", 0);
 
-        player=MediaPlayer.create(MainActivity.this,R.raw.beep);
+        player = MediaPlayer.create(MainActivity.this, R.raw.beep);
 
-        countDisplay = (TextView)findViewById(R.id.countDisplay);
+        countDisplay = (TextView) findViewById(R.id.countDisplay);
 
-        maxDuration = settingPref.getInt("maxDuration",3100);
+        maxDuration = settingPref.getInt("maxDuration", 3100);
 
         //configure proximity sensor
         sm = (SensorManager) getSystemService(SENSOR_SERVICE);
         proximitySensor = sm.getDefaultSensor(Sensor.TYPE_PROXIMITY);
         sm.registerListener(this, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
         //configure TTS
-        tts=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
-                if(status != TextToSpeech.ERROR) {
+                if (status != TextToSpeech.ERROR) {
                     tts.setLanguage(Locale.US);
                 }
 
 
-
-
-        //initial launch file
-        sentinel = sharedPref.getInt("formattingSentinel", 0);
-        if(sentinel == 0 ){
-            editor.putInt("formattingSentinel",1);
-            editor.apply();
-        }else{
-            numberOfSaves = sharedPref.getInt("numberSaves", 1);
-        }
-
-
+                //initial launch file
+                sentinel = sharedPref.getInt("formattingSentinel", 0);
+                if (sentinel == 0) {
+                    editor.putInt("formattingSentinel", 1);
+                    editor.apply();
+                } else {
+                    numberOfSaves = sharedPref.getInt("numberSaves", 1);
+                }
             }
         });
 
@@ -150,24 +145,24 @@ public class MainActivity extends Activity implements SensorEventListener {
     }
 
     //retrieve highscore
-    private int getHighscore(){
+    private int getHighscore() {
         int highscore = savePref.getInt("highscore", 1);
         return highscore;
     }
 
     //track duration between each push-up. tts reminder when slowing down
-    private void trackEncouragement(){
-        if (isTrackingDuration){
+    private void trackEncouragement() {
+        if (isTrackingDuration) {
             isTrackingDuration = false;
             endTime = new DateTime();
-            Duration dur = new Duration(startTime,endTime);
+            Duration dur = new Duration(startTime, endTime);
 
             long milliseconds = dur.getMillis();
 
-            if (milliseconds >= maxDuration){
-                tts.speak("Your Slowing down, keep it up",TextToSpeech.QUEUE_FLUSH,null);
+            if (milliseconds >= maxDuration) {
+                tts.speak("Your Slowing down, keep it up", TextToSpeech.QUEUE_FLUSH, null);
             }
-        }else{
+        } else {
             isTrackingDuration = true;
             startTime = new DateTime();
         }
@@ -180,32 +175,29 @@ public class MainActivity extends Activity implements SensorEventListener {
     protected void onPause() {
         super.onPause();
 
-        if (this.isFinishing()){
-            if(player != null){
+        if (this.isFinishing()) {
+            if (player != null) {
                 player.stop();
             }
             proximitySensor = null;
             sm = null;
-            if(tts !=null){
+            if (tts != null) {
                 tts.stop();
                 tts.shutdown();
             }
-        }else{
-            if(player != null){
+        } else {
+            if (player != null) {
                 player.stop();
 
             }
             proximitySensor = null;
             sm = null;
-            if(tts !=null){
+            if (tts != null) {
                 tts.stop();
                 tts.shutdown();
             }
 
         }
-
-
-
 
 
     }
@@ -214,52 +206,53 @@ public class MainActivity extends Activity implements SensorEventListener {
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
+
     //Proximity sensor to detect push-ups
     @Override
     public void onSensorChanged(SensorEvent event) {
 
-            for (int i = 0; i<1;i++){
-                if(event.values[0] <= 3.0f) {
+        for (int i = 0; i < 1; i++) {
+            if (event.values[0] <= 3.0f) {
 
-                    if (player != null && proximitySensor != null) {
-                        numberOfPushUps++;
-                        if (settingPref.getInt("encouragementSetting",1) == 1){
-                            trackEncouragement();
-                        }
-                        //check highscore
-                        if (numberOfPushUps>getHighscore()){
-                            saveEditor.putInt("highscore",numberOfPushUps);
-                            saveEditor.commit();
-                        }
-                        //determine if goal has been reached
-                        if (numberOfPushUps == goalValue) {
-                            int check = settingPref.getInt("voiceSetting",1);
-                            if (check == 1){
-                                String name = settingPref.getString("name","set name");
-
-                                tts.speak("Goal Reached nice job "+ name, TextToSpeech.QUEUE_FLUSH, null);
-                            }else{
-                                player.start();
-                            }
-
-                        }else {
-                            if (soundSwitch.isChecked()) {
-                                player.start();
-                            }
-
-                        }
-
-
-                        if(vibrateSwitch.isChecked() && proximitySensor != null){
-                            ((Vibrator)getSystemService(VIBRATOR_SERVICE)).vibrate(50);
-                        }
-                        countDisplay.setText(String.valueOf(numberOfPushUps));
-                    }else{
-                        player=MediaPlayer.create(MainActivity.this,R.raw.beep);
+                if (player != null && proximitySensor != null) {
+                    numberOfPushUps++;
+                    if (settingPref.getInt("encouragementSetting", 1) == 1) {
+                        trackEncouragement();
                     }
-                }
+                    //check highscore
+                    if (numberOfPushUps > getHighscore()) {
+                        saveEditor.putInt("highscore", numberOfPushUps);
+                        saveEditor.commit();
+                    }
+                    //determine if goal has been reached
+                    if (numberOfPushUps == goalValue) {
+                        int check = settingPref.getInt("voiceSetting", 1);
+                        if (check == 1) {
+                            String name = settingPref.getString("name", "set name");
 
+                            tts.speak("Goal Reached nice job " + name, TextToSpeech.QUEUE_FLUSH, null);
+                        } else {
+                            if (player != null) player.start();
+                        }
+
+                    } else {
+                        if (soundSwitch.isChecked()) {
+                            if (player != null) player.start();
+                        }
+
+                    }
+
+
+                    if (vibrateSwitch.isChecked() && proximitySensor != null) {
+                        ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(50);
+                    }
+                    countDisplay.setText(String.valueOf(numberOfPushUps));
+                } else {
+                    player = MediaPlayer.create(MainActivity.this, R.raw.beep);
+                }
             }
+
+        }
 
     }
 
@@ -269,40 +262,39 @@ Button Clicks
  */
 
     public void countUpButtonPressed(View view) {
-        if (settingPref.getInt("encouragementSetting",1) == 1){
+        if (settingPref.getInt("encouragementSetting", 1) == 1) {
             trackEncouragement();
         }
         numberOfPushUps++;
         countDisplay.setText(String.valueOf(numberOfPushUps));
 
         //check highscore
-        if (numberOfPushUps>getHighscore()){
-            saveEditor.putInt("highscore",numberOfPushUps);
+        if (numberOfPushUps > getHighscore()) {
+            saveEditor.putInt("highscore", numberOfPushUps);
             saveEditor.commit();
         }
 
-        if(vibrateSwitch.isChecked() && proximitySensor != null){
-        ((Vibrator)getSystemService(VIBRATOR_SERVICE)).vibrate(80);
+        if (vibrateSwitch.isChecked() && proximitySensor != null) {
+            ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(80);
         }
 
-        if(player != null && proximitySensor != null && soundSwitch.isChecked()) {
+        if (player != null && proximitySensor != null && soundSwitch.isChecked()) {
             if (numberOfPushUps == goalValue) {
-                int check = settingPref.getInt("voiceSetting",1);
-                if (check == 1){
-                    String name = settingPref.getString("name","set name");
+                int check = settingPref.getInt("voiceSetting", 1);
+                if (check == 1) {
+                    String name = settingPref.getString("name", "set name");
                     tts.speak("Goal Reached nice job " + name, TextToSpeech.QUEUE_FLUSH, null);
 
-                }else{
+                } else {
                     player.start();
                 }
 
-            }else {
+            } else {
                 player.start();
             }
-        }else{
-            player=MediaPlayer.create(MainActivity.this,R.raw.beep);
+        } else {
+            player = MediaPlayer.create(MainActivity.this, R.raw.beep);
         }
-
     }
 
     public void savedButtonClicked(View view) {
@@ -310,23 +302,22 @@ Button Clicks
         //Format text to be saved
         if (numberOfPushUps >= goalValue) {
             savePushUpFile = numberOfSaves + "." + " " + "On " + formattedDate + " you did : " + numberOfPushUps + " Push-Ups, GOAL REACHED";
-        }else{
+        } else {
             savePushUpFile = numberOfSaves + "." + " " + "On " + formattedDate + " you did : " + numberOfPushUps + " Push-Ups";
         }
 
         //Save number of push-ups done that session
-        editor.putInt(String.valueOf(numberOfSaves),numberOfPushUps);
+        editor.putInt(String.valueOf(numberOfSaves), numberOfPushUps);
         editor.apply();
 
-        for (int i = 1; i < numberOfSaves; i++){
+        for (int i = 1; i < numberOfSaves; i++) {
             int j = sharedPref.getInt(String.valueOf(i), 0);
-            if (j>getHighscore()){
-                int highscore = sharedPref.getInt(String.valueOf(i),0);
-                saveEditor.putInt("highscore",highscore);
+            if (j > getHighscore()) {
+                int highscore = sharedPref.getInt(String.valueOf(i), 0);
+                saveEditor.putInt("highscore", highscore);
                 saveEditor.commit();
             }
         }
-
 
 
         Toast.makeText(this, savePushUpFile, Toast.LENGTH_SHORT).show();
@@ -335,7 +326,7 @@ Button Clicks
         editor.putInt("numberSaves", numberOfSaves);
         editor.apply();
 
-        player.stop();
+        if (player != null) player.stop();
         proximitySensor = null;
         player = null;
         sm = null;
