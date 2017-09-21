0a286367fda0e272bac3b5bcf97528adc6e447ee
scott quach <scottqglobal@gmail.com>
2017-09-20 22:02:53 -0700

    Added reboot receiver

---
+++ app/src/main/java/com/awesome/scottquach/proximitypush_upcounter/RebootReceiver.java
@@ -0,0 +1,16 @@
+package com.awesome.scottquach.proximitypush_upcounter;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Scott Quach on 9/20/2017.
+ */
+
+public class RebootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
