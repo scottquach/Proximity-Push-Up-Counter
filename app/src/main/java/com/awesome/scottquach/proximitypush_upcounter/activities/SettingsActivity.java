df5327f9c298ebfc04f981ddcf138aba4c4e2329
Scott Quach <scottqglobal@gmail.com>
2019-05-11 10:15:36 -0700

    Made ad policy link look nice

--- app/src/main/java/com/awesome/scottquach/proximitypush_upcounter/activities/SettingsActivity.java
+++ app/src/main/java/com/awesome/scottquach/proximitypush_upcounter/activities/SettingsActivity.java
@@ -3,7 +3,9 @@ package com.awesome.scottquach.proximitypush_upcounter.activities;
 import android.app.NotificationManager;
 import android.app.TimePickerDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -101,6 +103,7 @@ public class SettingsActivity extends AppCompatActivity {
                 prefEditor.commit();
             }
         });
+
     }
 
     /**
@@ -203,4 +206,8 @@ public class SettingsActivity extends AppCompatActivity {
 
     }
 
+    public void onAdPolicyClicked(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://admob-app-id-2446206781.firebaseapp.com"));
+        startActivity(browserIntent);
+    }
 }
